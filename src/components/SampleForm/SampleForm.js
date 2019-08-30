bf135bd7545311ca26092d5e885470370fc1b85c
Sherwin Yu <sherwinhyu@gmail.com>
2019-08-30 00:08:55 -0400

    update carbon, update styling of form

--- src/components/SampleForm/SampleForm.js
+++ src/components/SampleForm/SampleForm.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Form, TextInput, Button } from 'carbon-components-react';
+import styled from 'styled-components';
+
+const StyledTextInput = styled(TextInput)`
+  margin-bottom: 15px;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  margin-bottom: 10px;
+`;
 
 const SampleForm = ({ handleChange, handleSubmit }) => (
   <div>
-    <h1>Sample Form</h1>
+    <Title>Sample Form</Title>
     <Form onSubmit={handleSubmit}>
-      <TextInput
+      <StyledTextInput
         name="firstName"
         labelText="First Name"
         helperText="Enter First name"
         onChange={handleChange}
         required
       />
-      <TextInput name="lastName" labelText="Last Name" helperText="Enter Last name" onChange={handleChange} required />
+      <StyledTextInput
+        id="lastName"
+        name="lastName"
+        labelText="Last Name"
+        helperText="Enter Last name"
+        onChange={handleChange}
+        required
+      />
+      <StyledTextInput
+        id="email"
+        name="email"
+        type="email"
+        labelText="Email"
+        helperText="Enter email"
+        onChange={handleChange}
+        require
+      />
       <Button type="submit">Submit</Button>
     </Form>
   </div>
--- src/components/SampleForm/SampleFormContainer.js
+++ src/components/SampleForm/SampleFormContainer.js
@@ -25,9 +25,9 @@ class ContactContainer extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6 offset-md-3">
+      <div className="bx--grid">
+        <div className="bx--row">
+          <div className="bx--col-xs-12 bx--col-sm-12 bx--col-md-8 bx--offset-md-2 bx--col-lg-6 bx--offset-lg-3 bx--col-xl-8 bx--offset-xl-2 bx--col-xxl-8 bx--offset-xxl-2">
             <SampleForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
           </div>
         </div>
