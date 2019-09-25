0956a5e80d9df5b42a29bfe4a907f860f49c8fc0
Sherwin Yu <sherwinhyu@gmail.com>
2019-09-24 22:30:24 -0400

    adjust responsiveness

--- src/components/SampleForm/SampleFormContainer.js
+++ src/components/SampleForm/SampleFormContainer.js
@@ -65,7 +65,7 @@ class FormContainer extends Component {
     return (
       <div className="bx--grid">
         <div className="bx--row">
-          <div className="bx--col-xs-12 bx--col-sm-12 bx--col-md-8 bx--offset-md-2 bx--col-lg-6 bx--offset-lg-3 bx--col-xl-8 bx--offset-xl-2 bx--col-xxl-8 bx--offset-xxl-2">
+          <div className="bx--col-xs-12 bx--col-sm-12 bx--col-md-8 bx--offset-md-2 bx--col-lg-8 bx--offset-lg-2 bx--col-xl-10 bx--offset-xl-1 bx--col-xxl-10 bx--offset-xxl-1">
             <SampleForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
             <Users
               users={users}
